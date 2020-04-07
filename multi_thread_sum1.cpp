e7934af7649609bbed58a1294100d2f5f417abcd
root <a13594077248@gmail.com>
2020-04-07 11:25:37 +0800

    增加代码，使其可处理非整除情况

--- multi_thread_sum1.cpp
+++ multi_thread_sum1.cpp
@@ -26,15 +26,17 @@ void* sum_job(void* threadarg)    //每个子线程处理一个部分
     for(i=start; i <= end; i++){
         sum[n] = sum[n] + i;
     }
+     printf("线程id是 %d,start = %d,end = %d,计算结果为 %d \n",n,start,end,sum[n]);
+
     pthread_exit(0);
 }
 
 int main(){
     int N;
     int M;
-    cout << "Please input thread number\n";
+    cout << "Please input thread number:\n";
     cin >> N;
-    cout << "Please input count number\n";
+    cout << "Please input count number:\n";
     cin >> M;
     pthread_t *pthread_id = NULL;
     thread_data *td = new thread_data[N];
@@ -54,7 +56,18 @@ int main(){
         pthread_join(pthread_id[i],NULL);
         result += sum[i];
     }
-
+//以下部分在主线程计算剩余未分配的数组
+    int main_start;
+    int main_end;
+    int main_sum;
+    main_sum = 0;
+    main_start = N*aveN+1;
+    main_end = M;
+    for (i=main_start;i<main_end+1;i++){
+        main_sum = main_sum+i;
+    }
+    printf("main_start = %d,main_end = %d,main_sum = %d\n",main_start,main_end,main_sum);
+    result = result+main_sum;
     printf("sum is %d\n",result);
 
     free(pthread_id);
