ca358da25e6f3d07f3d298b52a520a6dddc2b1fa
root <a13594077248@gmail.com>
2020-04-29 20:32:17 +0800

    openmp

--- homework7.cpp
+++ homework7.cpp
@@ -1,30 +1,31 @@
 #include <bits/types/clock_t.h>
+#include <ctime>
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include<ctime>
 #include<iostream>
 using namespace std;
 int main(int argc,char**argv)
 {
     clock_t startTime,endTime;
-    startTime = clock();//计时开始
+    startTime = clock();
     int steps=atoi(argv[1]);
     double step = 1.0/steps;
     double sum  = 0,x;
     int i;
     //#pragma omp parallel for
-    //omp_lock_t lock;
-    //omp_init_lock(&lock);
+    omp_lock_t lock;
+    omp_init_lock(&lock);
     for(i=0;i<=steps;i++){
-      //  omp_set_lock(&lock);
+        omp_set_lock(&lock);
         x = (i+0.5)*step;
     //#pragma omp critical
         sum = sum+4/(1+x*x)*step;
-     //   omp_unset_lock(&lock);
+        omp_unset_lock(&lock);
     }
-    //omp_destroy_lock(&lock);
+    omp_destroy_lock(&lock);
     printf("pi = %1.20f\n",sum);
     endTime = clock();
-    cout << "The run time is:" << (double)clock() /CLOCKS_PER_SEC<<"s"<<endl;
+    cout << "The run time is: " <<(double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+    return 0;
 }
